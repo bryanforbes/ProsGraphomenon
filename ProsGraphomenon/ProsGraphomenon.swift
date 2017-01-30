ff4d48afb6920fb4e2d8ad4d713c7137f547012c
Bryan Forbes <bryan@reigndropsfall.net>
2017-01-30 08:55:24 -0600

    Set up frameworks and build

---
+++ ProsGraphomenon/ProsGraphomenon.swift
@@ -0,0 +1,14 @@
+//
+//  ProsGraphomenon.swift
+//  ProsGraphomenon
+//
+//  Created by Bryan Forbes on 1/30/17.
+//  Copyright © 2017 Bryan Forbes. All rights reserved.
+//
+
+import Foundation
+
+
+class ProsGraphomenonPrincipalClass: NSObject, THOPluginProtocol {
+	
+}
